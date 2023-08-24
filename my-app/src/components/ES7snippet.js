8682c8808780c752372270858739b2269b1ea526
Myekman <my@traningstemplet.se>
2023-08-24 12:16:56 +0000

    ES7 install and learning

---
+++ my-app/src/components/ES7snippet.js
@@ -0,0 +1,55 @@
+// ...........................................................rfce and efter 
+
+// import React from 'react'
+
+// function ES7snippet() {
+//   return (
+//     <div>ES7snippet</div>
+//   )
+// }
+
+// export default ES7snippet
+
+// ............................................................rafce and enter
+
+// import React from 'react'
+
+// const ES7snippet = () => {
+//   return (
+//     <div>ES7snippet</div>
+//   )
+// }
+
+// ...........................................................rce enter
+
+// export default ES7snippet
+
+// import React, { Component } from 'react'
+
+// export class ES7snippet extends Component {
+//   render() {
+//     return (
+//       <div>ES7snippet</div>
+//     )
+//   }
+// }
+
+// export default ES7snippet
+
+// .............................................................aconst and enter
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// .............................................................clg and enter
+
+console.log(first)
+
+// .............................................................clo and enter
+
+console.log('first', first)
