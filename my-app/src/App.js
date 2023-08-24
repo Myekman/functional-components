50f453b2271d7aeff2b5707e4d7e1ee9c4d7eafb
Myekman <my@traningstemplet.se>
2023-08-24 11:59:58 +0000

    react learning

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
 import StatefulGreeting from './components/StatefulGreeting';
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* Add your components here */}
       <Sidebar />
       <StatefulGreeting greeting="im a stateful class component" />
+      <NavBarSimple />
     </div>
   );
 }
